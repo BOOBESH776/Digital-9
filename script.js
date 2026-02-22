13fc1109ee8aa3525b8835c03c4e8b37fb2b6ac6
BOOBESH776 <cboobesh776@gmail.com>
2026-02-22 17:09:28 +0530

    make counter

---
+++ script.js
@@ -0,0 +1,17 @@
+var client = document.getElementById("ClientsServed");
+var data = document.getElementById("RawData");
+var rec = document.getElementById("Recommendation");
+
+let count = 0;
+
+let upto = 0;
+let counts = setInterval(updated, 1);
+
+function updated() {
+    let count = document.getElementById("ClientsServed");
+    count.innerHTML = ++upto;
+    if (upto === 499) {
+        clearInterval(counts);
+    }
+}
+
