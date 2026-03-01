cea38ae200c063f586a93b3407ecb12c77db3a60
BOOBESH776 <cboobesh776@gmail.com>
2026-03-01 14:49:09 +0530

    price-selection,and change some height

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var data = document.getElementById("RawData");
-var rec = document.getElementById("Recommendation");
+// var data = document.getElementById("RawData");
+// var rec = document.getElementById("Recommendation");
 
-let count = 0;
+// let count = 0;
 
-let upto = 0;
-let counts = setInterval(updated, 1);
-function updated() {
-    let count = document.getElementById("ClientsServed");
-    count.innerHTML = ++upto;
-    if (upto === 499) {
-        clearInterval(counts);
-    }
-}
+// let upto = 0;
+// let counts = setInterval(updated, 1);
+// function updated() {
+//     let count = document.getElementById("ClientsServed");
+//     count.innerHTML = ++upto;
+//     if (upto === 499) {
+//         clearInterval(counts);
+//     }
+// }
+
+
+let price = document.getElementById("price-colum");
