1b59db4fb92a4a3d0f4f726df239cb000444b8ab
BOOBESH776 <cboobesh776@gmail.com>
2026-03-14 18:07:44 +0530

    make testimonials section

--- script.js
+++ script.js
@@ -70,3 +70,34 @@ plans.forEach(function (features) {
     col.appendChild(arrowBtn);
     container.appendChild(col);
 });
+
+// testimonial
+
+const multipleItemCarousel = document.querySelector("#testimonialCarousel");
+
+if (window.matchMedia("(min-width:576px)").matches) {
+    const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+        interval: false
+    });
+
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+
+    var scrollPosition = 0;
+
+    $(".carousel-control-next").on("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 3) {
+            console.log("next");
+            scrollPosition = scrollPosition + cardWidth;
+            $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800);
+        }
+    });
+    $(".carousel-control-prev").on("click", function () {
+        if (scrollPosition > 0) {
+            scrollPosition = scrollPosition - cardWidth;
+            $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800);
+        }
+    });
+} else {
+    $(multipleItemCarousel).addClass("slide");
+}
